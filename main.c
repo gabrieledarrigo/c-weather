4af3576daeb11e2309bc04addb2106255922aa10
Gabriele D'Arrigowq <darrigo.g@gmail.com>
2018-01-14 19:48:51 +0100

    Function to read data from the open weather api

--- main.c
+++ main.c
@@ -1,6 +1,7 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
+#include <curl/curl.h>
 #include "./lib/jsmn.h"
 
 /**
@@ -59,26 +60,7 @@ jsmntok_t * parse_json(char * data) {
     return tokens;
 }
 
-int main(int argc, char *argv[]) {
-    printf("Number of argc: %i\n", argc);
-
-    if (argc == 1) {
-        printf("Please specify the name of a city\n");
-        exit(0);
-    } else {
-        printf("Search for weather forecast in: %s\n", argv[1]);
-    }
-
-    char * data;
-    jsmntok_t * tokens;
-    
-    // Read data from file
-    data = read_file();
-    // Parse the json
-    tokens = parse_json(data);
-
-    printf("Tokens size: %i\n", tokens[0].size);
-
+int search_city(char * to_search, char * data, jsmntok_t * tokens) {
     //Iterate the json array to find the desired city.
     for (int i = 1; i <= tokens[0].size; i++) {
         int length = tokens[i].end - tokens[i].start;
@@ -86,15 +68,96 @@ int main(int argc, char *argv[]) {
         strncpy(city, data + tokens[i].start, length);
         city[length] = '\0';
 
-        printf("To search: %s - Actual: %s\n", argv[1], city);
-
-        if (strcmp(argv[1], city) == 0) {
+        if (strcmp(to_search, city) == 0) {
             printf("Found! %s\n", city);
-            break;
+            return 1;
         }
     }
 
-    printf("Searched city: %s not found\n", argv[1]);
+    return 0;
+}
+
+struct Buffer {
+    char *memory;
+    size_t size;
+};
+
+size_t write_to_memory(void *data, size_t size, size_t nmemb, void *pointer) {
+    size_t real_size = size * nmemb;
+
+    // cast the received pointer to the Buffer structured data.
+    struct Buffer * buffer = (struct Buffer *) pointer;
+
+    // reallocate the memory for the Buffer char pointer
+    buffer->memory = realloc(buffer->memory, buffer->size + real_size);
+    buffer->size += real_size;
+
+    if (buffer->memory == NULL) {
+        printf("Failed to allocate memory for the http get data");
+        return 1;
+    }
+
+    // Copy the fetched data to the Buffer char pointer
+    memcpy(buffer->memory, data, real_size);
+    buffer->memory[real_size] = '\0';
+}
+
+char * get_weather_data(char * city) {
+    curl_global_init(CURL_GLOBAL_ALL);
+
+    char url[] = "http://api.openweathermap.org/data/2.5/weather?appid=82390064987812e43f9ec57cb01311b6&q=";
+    strcat(url, city);
+
+    printf("%s\n", url);
+
+    struct Buffer buffer;
+    buffer.memory = malloc(1);
+    buffer.size = 0;
+
+    CURL *handle;
+    CURLcode result;
+    handle = curl_easy_init();
+
+    curl_easy_setopt(handle, CURLOPT_URL, url);
+    curl_easy_setopt(handle, CURLOPT_WRITEFUNCTION, write_to_memory);
+    curl_easy_setopt(handle, CURLOPT_WRITEDATA, (void *) &buffer);
+    result = curl_easy_perform(handle);
+
+    return buffer.memory;
+};
+
+
+int main(int argc, char *argv[]) {
+    if (argc == 1) {
+        printf("Please specify the name of a city to search for\n");
+        exit(0);
+    } else {
+        printf("Search for weather forecast in: %s\n", argv[1]);
+    }
+
+    int found;
+    char * data;
+    jsmntok_t * tokens;
+    
+    // Read data from file
+    data = read_file();
+    // Parse the json
+    tokens = parse_json(data);
+    // Search the city
+    found = search_city(argv[1], data, tokens);
+
+    //printf("Tokens size: %i\n", tokens[0].size);
+
+    if (!found) {
+        printf("%s was not found in the archive. Please provide another city to search for\n", argv[1]);
+        exit(0);
+    }
+
+    printf("Searched city: \"%s\" was found\n", argv[1]);  
+
+
+    get_weather_data( argv[1]);
+
     free(data);
     free(tokens);
     return 0;
